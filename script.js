73462b0f780b7fc93dffcb69f97e34734641d3aa
Marimerlim <mariri.merlim@gmail.com>
2022-06-15 12:21:06 -0300

    desafio pronto!

---
+++ script.js
@@ -0,0 +1,25 @@
+//Desafio 01 - Estrutura de Dados
+//aproveitando o código usado na aula
+
+
+// Crie uma variável que receba um nome;
+// Crie uma variável que receba um número;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+// Mostre no console a mensagem "Estou mexendo no console.log";
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+
+let clima = "Está frio!"
+console.log (clima)
+
+let graus = 19;
+let anoAtual = 2022 - graus;
+console.log (anoAtual)
+
+let texto = "Estou mexendo no console.log"
+console.log (texto)
+
+let quartaFeira = true;
+console.log(typeof quartaFeira)
